3a19a748acc332db1a2eb5f359c2ef5c009b05c3
18Prashanth <18gowda2002@gmail.com>
2024-12-21 12:55:02 +0530

    airthmatic_operator

---
+++ 5_Airthmaticopo.py
@@ -0,0 +1,47 @@
+# # Airthmatic operators
+
+# friends = 5
+
+# # # friends = friends + 1
+# # # print(friends)
+
+# # # augmented assignment operators
+# # friends += 2
+# # print(friends)
+
+# # friends = friends + 1
+# friends += 1
+# print(friends)
+
+# # friends = friends - 2
+# friends -= 2
+# print(friends)
+
+# # friends = friends * 2
+# friends *= 2
+# print(friends)
+
+# # friends = friends / 2
+# friends /= 2
+# print(friends)
+
+# # friends = friends**2
+# friends **= 2
+# print(friends)
+
+# # friends = friends % 2
+# friends %= 2
+# print(friends)
+
+x = 3.14
+y = -4
+z = 5
+
+# result = round(x)
+# result = abs(y)
+# result = pow(4, 3)
+result = max(x, y, z)
+# result = min(x, y, z)
+
+
+print(result)
