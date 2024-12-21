d5d4833d326b2432ac37718aaeca893c94f41dad
18Prashanth <18gowda2002@gmail.com>
2024-12-21 13:44:00 +0530

    math_function

---
+++ 6_Math function.py
@@ -0,0 +1,39 @@
+# import math
+
+# x = 9.5
+
+# print(math.pi)
+# print(math.e)
+# print(math.sqrt(x))
+# print(math.ceil(x))
+# print(math.floor(x))
+
+# program to calculate the circumference of the circle
+# import math
+
+# # pi = math.pi
+# radius = int(input("Enter the radius of the circel: "))
+
+# c = round(2 * math.pi * radius, 2)
+
+# print(f"circumference of a circel is {c}")
+
+
+# # program to calculate the area of a circle
+# import math
+
+# radius = float(input("Enter the radius of a circle: "))
+
+# result = math.pi * pow(radius, 2)
+
+# print(f"The area of the circle is {round(result, 2)}cm")
+
+
+# program to find the hypothese of a triangle
+import math
+
+a = float(input("Enter the value of a: "))
+b = float(input("Enter the value of b: "))
+
+c = math.sqrt(pow(a, 2) + pow(b, 2))
+print(f"The hypothesis of a triangle is: {c}")
