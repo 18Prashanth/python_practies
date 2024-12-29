db5b6cf19e49445c11b9ca61ad737ef259dd930e
18Prashanth <18gowda2002@gmail.com>
2024-12-29 22:44:02 +0530

    update

--- 11_Logial operator.py
+++ 11_Logial operator.py
@@ -4,10 +4,35 @@
 #                   not = inverts the condition (not False, not True)
 
 
-temp = -1
-is_raining = False
+# temp = 35
+# is_raining = False
 
-if temp > 35 or temp < 0 or is_raining:
-    print("The outdoor event is cancel")
-else:
-    print("The outdoor event is still scheduled")
+# if temp > 35 or temp < 0 or is_raining:
+#     print("The outdoor event is cancel")
+# else:
+#     print("The outdoor event is still scheduled")
+
+
+# example
+
+temp = 0
+is_sunny = True
+
+if temp >= 28 and is_sunny:
+    print("It is HOT outside😁")
+    print("It is sunny")
+elif temp <= 0 and is_sunny:
+    print("It is COLD outside")
+    print("It is SUNNY")
+elif 28 > temp > 0 and is_sunny:
+    print("It is WARM outside")
+    print("It is SUNNY")
+elif temp >= 28 and not is_sunny:
+    print("It is HOT outside😁")
+    print("It is CLOUDY")
+elif temp <= 0 and not is_sunny:
+    print("It is CLOUD outside")
+    print("It is CLOUDY")
+elif 28 > temp > 0 and not is_sunny:
+    print("It is WARM outside")
+    print("It is WARM")
