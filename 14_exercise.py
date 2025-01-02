b2173503d2ef878a1bfe5a5fcdefab5aa6236d8e
18Prashanth <18gowda2002@gmail.com>
2025-01-02 17:25:30 +0530

    exercise_

--- 14_exercise.py
+++ 14_exercise.py
@@ -7,3 +7,9 @@ username = input("Enter the user name: ")
 
 if len(username) > 12:
     print("Your user name can't be more than 12 character")
+elif not username.find(" ") == -1:
+    print("Your username can't contain spaces")
+elif not username.isalpha():
+    print("Your username can't contain number")
+else:
+    print(f"Welcome {username}!")
