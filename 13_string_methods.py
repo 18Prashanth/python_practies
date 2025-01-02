a19d0d920791afa52ffc9b55812cc597028d68e7
18Prashanth <18gowda2002@gmail.com>
2025-01-02 13:54:57 +0530

    String update

--- 13_string_methods.py
+++ 13_string_methods.py
@@ -1,4 +1,10 @@
+# name.len() = This function is used to get the length of the string
+# name.find() = This function is used to find the string which is return first occurance
+# name.rfind() = This function is used to find the string which is return last occurance
+
 name = input("Enter your full name: ")
 
-result = len(name)
+# result = len(name)
+# result = name.find("a")
+result = name.rfind("a")
 print(result)
