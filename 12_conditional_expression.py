599caa4003de0b81f953d2f78ac4f6bb400f97fa
18Prashanth <18gowda2002@gmail.com>
2025-01-02 12:46:45 +0530

    conditional_updated

--- 12_conditional_expression.py
+++ 12_conditional_expression.py
@@ -4,21 +4,24 @@
 
 
 num = 5
-
 a = 6
 b = 7
 age = 13
 temperature = 30
+user_role = "Admin"
 
 
-# print("Positive" if num > 0 else "Negative")
-# result = "EVEN" if num % 2 == 0 else "ODD"
-# max_num = a if a > b else b
-# min_num = a if a < b else b
-# status = "ADULT" if age >= 18 else "CHILD"
+print("Positive" if num > 0 else "Negative")
+result = "EVEN" if num % 2 == 0 else "ODD"
+max_num = a if a > b else b
+min_num = a if a < b else b
+status = "ADULT" if age >= 18 else "CHILD"
 wheather = "HOT" if temperature > 20 else "COLD"
+access_level = "Full Access" if user_role == "Admin" else "Limited Access"
 
-# print(f"The maximum number is: {max_num} ")
-# print(f"The minimum number is: {min_num} ")
-# print(status)
+
+print(f"The maximum number is: {max_num} ")
+print(f"The minimum number is: {min_num} ")
+print(status)
 print(wheather)
+print(access_level)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,4 @@
-2
+print(f"The maximum number is: {max_num} ")
+print(f"The minimum number is: {min_num} ")
+print(status)
+print(wheather)
