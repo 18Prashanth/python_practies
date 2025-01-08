ae9fa22a89144c6549f7c7da91c3950abbec1e5c
18Prashanth <18gowda2002@gmail.com>
2025-01-08 22:04:10 +0530

    string upadet

--- 15_Formate_specifier.py
+++ 15_Formate_specifier.py
@@ -5,6 +5,19 @@ price1 = 3.14159
 price2 = -987.65
 price3 = 12.34
 
-print(f"Price 1 is ${price1:.2f}")
-print(f"Price 2 is ${price2:.2f}")
-print(f"Price 3 is ${price3:.2f}")
+# print(f"Price 1 is ${price1:.1f}")
+# print(f"Price 2 is ${price2:.1f}")
+# print(f"Price 3 is ${price3:.1f}")
+
+# print(f"Price 1 is ${price1: 10}")
+# print(f"Price 2 is ${price2: 10}")
+# print(f"Price 3 is ${price3: 10}")
+
+
+# print(f"Price 1 is ${price1: 010}")
+# print(f"Price 2 is ${price2: 010}")
+# print(f"Price 3 is ${price3: 010}")
+
+# print(f"Price 1 is ${price1: <10}")
+# print(f"Price 2 is ${price2: <10}")
+# print(f"Price 3 is ${price3: <10}")
