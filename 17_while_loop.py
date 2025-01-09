1f0d44a9bd60af1fdc28721f643a26625b1410df
18Prashanth <18gowda2002@gmail.com>
2025-01-09 13:34:54 +0530

    while_loop

--- 17_while_loop.py
+++ 17_while_loop.py
@@ -1,1 +1,8 @@
 # while loop = execute some code  WHILE some conditions remains true
+
+name = input("Enter your name: ")
+
+while name == "":
+    print("You didi not enter your name")
+    name = input("Enter your name: ")
+print(f"Hello {name}")
