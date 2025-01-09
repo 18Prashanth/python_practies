46e1962efd159ef35428a688bdaad1f47104cb07
18Prashanth <18gowda2002@gmail.com>
2025-01-09 14:14:36 +0530

    while_loop

--- 17_while_loop.py
+++ 17_while_loop.py
@@ -20,9 +20,18 @@
 
 
 # example 3
-food = input("Enter a food ypu like (q to quit): ")
+# food = input("Enter a food ypu like (q to quit): ")
 
-while not food == 'q':
-    print(f"You like {food}")
-    food = input("Enter another food you like(q to quit):")
-print("bye")
+# while not food == 'q':
+#     print(f"You like {food}")
+#     food = input("Enter another food you like(q to quit):")
+# print("bye")
+
+
+# example 4
+num = int(input("Enter a # between 1-10"))
+
+while num < 1 or num > 10:
+    print(f"{num} is not valid")
+    num = int(input("Enter a # between 1-10"))
+print(f"Your number is {num}")
