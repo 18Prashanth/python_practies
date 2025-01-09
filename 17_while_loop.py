c41614e523b45451d5ad6dea7263e5a13a9d5cc7
18Prashanth <18gowda2002@gmail.com>
2025-01-09 13:50:41 +0530

    while_loop

--- 17_while_loop.py
+++ 17_while_loop.py
@@ -10,10 +10,19 @@
 
 
 # example 2
-age = int(input("Enter your age: "))
+# age = int(input("Enter your age: "))
 
-while age < 0:
-    print("Age can't be negative")
-    age = int(input("Enter your age: "))
+# while age < 0:
+#     print("Age can't be negative")
+#     age = int(input("Enter your age: "))
 
-print(f"you are {age} years old")
+# print(f"you are {age} years old")
+
+
+# example 3
+food = input("Enter a food ypu like (q to quit): ")
+
+while not food == 'q':
+    print(f"You like {food}")
+    food = input("Enter another food you like(q to quit):")
+print("bye")
