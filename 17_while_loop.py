6ed99059bac35a4e1e078babbd95ed18829d25ce
18Prashanth <18gowda2002@gmail.com>
2025-01-09 13:44:28 +0530

    while_loop

--- 17_while_loop.py
+++ 17_while_loop.py
@@ -1,8 +1,19 @@
 # while loop = execute some code  WHILE some conditions remains true
 
-name = input("Enter your name: ")
+# example 1
+# name = input("Enter your name: ")
 
-while name == "":
-    print("You didi not enter your name")
-    name = input("Enter your name: ")
-print(f"Hello {name}")
+# while name == "":
+#     print("You didi not enter your name")
+#     name = input("Enter your name: ")
+# print(f"Hello {name}")
+
+
+# example 2
+age = int(input("Enter your age: "))
+
+while age < 0:
+    print("Age can't be negative")
+    age = int(input("Enter your age: "))
+
+print(f"you are {age} years old")
