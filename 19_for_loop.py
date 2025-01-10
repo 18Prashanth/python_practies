bbeae7c97a9aefcc5c0803f5fd614bd418bfada4
18Prashanth <18gowda2002@gmail.com>
2025-01-10 21:24:06 +0530

    for_update

--- 19_for_loop.py
+++ 19_for_loop.py
@@ -1,2 +1,23 @@
 # for loops = execute a block of code a fixed number of ti,es.
 #           you can iterate over a range, string, sequence, etc.
+# for "variable" in range(start, end, difference)   start=0, end=end-1
+
+
+# for x in range(1, 10):
+#     print(x)
+
+
+# for x in reversed(range(1, 10, 2)):
+#     print(x)
+# print("HAPPY NEW YEAR!")
+
+
+# credit_card = "1234-5678-9012-3456"
+# for x in credit_card:
+#     print(x)
+
+for x in range(1, 21):
+    if x == 13:
+        break
+    else:
+        print(x)
