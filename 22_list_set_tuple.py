52d46afd0d552248c00e30d9be99953d233f7172
18Prashanth <18gowda2002@gmail.com>
2025-01-11 16:30:39 +0530

    collection

---
+++ 22_list_set_tuple.py
@@ -0,0 +1,4 @@
+# collection = single "variable" used to store multiple value
+# List = [] ordered and changeable. Duplocated OK
+# set = {} unordered and immutable, but Add/Remove ok, No duplicates
+# Tuple= () ordered and unchangeable. Duplicates ok. Faster
