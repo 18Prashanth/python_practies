73565ebf18bac56a16945bcce09a933d112c3a0b
18Prashanth <18gowda2002@gmail.com>
2025-01-11 16:26:45 +0530

    nested

--- 21_nested_loop.py
+++ 21_nested_loop.py
@@ -2,7 +2,27 @@
 #               outer loop:
 #                   inner loop:
 
-for x in range(0, 10):
-    for y in range(0, 10):
-        for z in range(0, 10):
-            print(f"( {x}, {y}, {z})", end="")
+# for x in range(0, 10):
+#     for y in range(0, 10):
+#         for z in range(0, 10):
+#             print(f"( {x}, {y}, {z})", end="")
+
+
+# for x in range(1, 10):
+#     print(x, end="")
+
+
+# for x in range(3):
+#     for y in range(1, 10):
+#         print(y, end="")
+#     print()
+
+
+rows = int(input("Enter the # of rows: "))
+columns = int(input("Enter the # of columns: "))
+symbol = input("Enter a symbol to use: ")
+
+for x in range(rows):
+    for y in range(columns):
+        print(symbol, end="")
+    print()
