3f1b4509ef3f7fa8473b730a9892a123013e1c84
18Prashanth <18gowda2002@gmail.com>
2025-01-11 16:18:10 +0530

    nested

--- 21_nested_loop.py
+++ 21_nested_loop.py
@@ -1,3 +1,8 @@
 # nested loop = A loop within another loop (outer, inner)
 #               outer loop:
 #                   inner loop:
+
+for x in range(0, 10):
+    for y in range(0, 10):
+        for z in range(0, 10):
+            print(f"( {x}, {y}, {z})", end="")
