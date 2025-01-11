47b6d3ef71e4193ef36bb29b23cfb1d2fb5dbc59
18Prashanth <18gowda2002@gmail.com>
2025-01-11 13:14:12 +0530

    count

--- 20_count_down.py
+++ 20_count_down.py
@@ -1,5 +1,11 @@
 import time
 
-time.sleep(3)
+# time.sleep(3)
 
-print("TIME'S UP!")
+# print("TIME'S UP!")
+
+my_time = int(input("Enter the time in seconds: "))
+
+for x in range(0,  my_time):
+    time.sleep(0)
+print("TIMES'S UP!")
