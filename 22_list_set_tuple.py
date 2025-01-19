5ae086f77832633f1745cc562028c2fd1190befb
18Prashanth <18gowda2002@gmail.com>
2025-01-19 14:52:28 +0000

    list

--- 22_list_set_tuple.py
+++ 22_list_set_tuple.py
@@ -2,3 +2,8 @@
 # List = [] ordered and changeable. Duplocated OK
 # set = {} unordered and immutable, but Add/Remove ok, No duplicates
 # Tuple= () ordered and unchangeable. Duplicates ok. Faster
+
+# collection
+
+fruit = "apple"
+print(fruit)
