b2be10c5f00015c5cd995cdc037aea8965941c8a
18Prashanth <18gowda2002@gmail.com>
2025-01-22 20:36:08 +0000

    2D_List

---
+++ 24_2D_List.py
@@ -0,0 +1,9 @@
+# 2dlist
+
+fruits = ["apple", "orange", "banana", "coconut"]
+vegetables = ["celery", "carrots", "potatoes"]
+meats = ["chicen", "fish", "turkey"]
+
+groceries = [fruits, vegetables, meats]
+
+print(groceries[1][1])
