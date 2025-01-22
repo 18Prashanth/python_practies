6a28c48b5672bce4c42292664f90cf4832c955f3
18Prashanth <18gowda2002@gmail.com>
2025-01-22 20:18:29 +0000

    shopping

--- 23_Shopping_cart.py
+++ 23_Shopping_cart.py
@@ -1,1 +1,14 @@
 # shopping cart program
+
+
+foods = []
+prices = []
+total = 0
+
+while True:
+    food = input("Enter a food to buy(Q to quit):")
+    if food.lower() == 'q':
+        break
+    else:
+        foods.append(food)
+print(foods)
