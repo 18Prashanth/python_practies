3f042d0f5b042f9cda9539f3aece70141f350f8e
18Prashanth <18gowda2002@gmail.com>
2025-01-23 12:38:55 +0000

    Quiz

--- 28_quiz_game.py
+++ 28_quiz_game.py
@@ -12,7 +12,7 @@ oprions = (("A:116", "B:117 ", "C:118 ", "D:119 "),
            ("A:206", "B:207 ", "C:208 ", "D:209 "),
            ("A:Mercury", "B:Venus ", "C:Earth ", "D:Mars "))
 
-answer = ("C", "D", "A", "A", "B")
+answers = ("C", "D", "A", "A", "B")
 guesses = []
 score = 0
 question_num = 0
@@ -25,9 +25,30 @@ for question in questions:
 
     guess = input("Enter (A, B, C, D): ").upper()
     guesses.append(guess)
-    if guess == answer[question_num]:
+    if guess == answers[question_num]:
         score += 1
         print("CORRECT!")
     else:
         print("INCORRECT!")
+        print(f"{answers[question_num]} is the correct answer")
     question_num += 1
+
+
+print("----------------------")
+print("     RESULT      ")
+print("----------------------")
+
+
+print("answers: ", end="")
+for answer in answers:
+    print(answer, end=" ")
+print()
+
+print("guesses: ", end="")
+for guess in guesses:
+    print(guess, end=" ")
+print()
+
+
+score = int(score / len(questions) * 100)
+print(f"Your score is: {score}%")
