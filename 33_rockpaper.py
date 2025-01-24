310021ed3f7e483330ddcfc916502de8f696e231
18Prashanth <18gowda2002@gmail.com>
2025-01-24 14:59:55 +0000

    rockpaper game

--- 33_rockpaper.py
+++ 33_rockpaper.py
@@ -1,1 +1,12 @@
 # python rock paper scissors game
+
+import random
+
+options = ("rock", "paper", "scissors")
+player = None
+computer = random.choice(options)
+
+while player not in options:
+    player = input("Enter a choice (rock, paper, scissors): ")
+print(f"Player: {player}")
+print(f"computer: {computer}")
