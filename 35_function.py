0a8f6ef026f91d2fbc753cdf9ce42063be07b87d
18Prashanth <18gowda2002@gmail.com>
2025-01-24 16:04:09 +0000

    function

--- 35_function.py
+++ 35_function.py
@@ -1,11 +1,13 @@
 # function = A block of reusable code
 # place () after the function name to invoke it
 
-def happy_birthday():
-    print("Happy birthday to you")
-    print("You are old")
+def happy_birthday(name, age):
+    print(f"Happy birthday to {name}")
+    print(f"You are {age} years of old")
     print("Happy birthday to you!")
     print()
 
 
-happy_birthday()
+happy_birthday("bro", 20)
+happy_birthday("smith", 24)
+happy_birthday("Joe", 20)
