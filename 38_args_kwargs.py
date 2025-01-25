0ab02d2fa7325bdb2acb7f6b78d8d9f9dbd7b83a
18Prashanth <18gowda2002@gmail.com>
2025-01-25 20:35:06 +0000

    args_kwargs

--- 38_args_kwargs.py
+++ 38_args_kwargs.py
@@ -38,12 +38,13 @@ def shipping_label(*args, **kwargs):
     for arg in args:
         print(arg, end=" ")
     print()
+
     for value in kwargs.values():
         print(value, end=" ")
     print()
 
-    print(f"{kwargs.get('street')}")
-    print(f"{kwargs.get('city')}, {kwargs.get('state')}, {kwargs.get('zip')}")
+    # print(f"{kwargs.get('street')}")
+    # print(f"{kwargs.get('city')}, {kwargs.get('state')}, {kwargs.get('zip')}")
 
 
 shipping_label("Dr", "Prashanth", "Gowda", "III",
