40d322a5abf96bf2c659f950e50114173c9a358a
18Prashanth <18gowda2002@gmail.com>
2025-01-25 14:17:13 +0000

    keyword argument

--- 37_keyword_argument.py
+++ 37_keyword_argument.py
@@ -2,3 +2,15 @@
 # helps with readability
 # order of arguments doesn't matter
 # 1. positional 2. default 3. KEYWORD 4. Arbitrary
+
+
+# def hello(greeting, title, first, last):
+#   print(f"{greeting} {title} {first} {last}")
+
+
+# hello("Hello", title="Mr.", first="spongebob", last="squarepants")
+
+
+# example - 2
+
+print("1", "2", "3", "4", sep="-")
