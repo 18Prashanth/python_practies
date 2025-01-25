c3a3049a8d84b3e84be0ff92c1fb893697507763
18Prashanth <18gowda2002@gmail.com>
2025-01-25 13:51:19 +0000

    default argument

--- 36_defaul_argument.py
+++ 36_defaul_argument.py
@@ -2,3 +2,23 @@
 #                   default is used when that argument is Omitted
 #                   make your fuunctions more flexible, reducces # of argumnets
 #                    1. positional, 2. DEFAULT, 3. Keyword, 4. ARbitrary
+
+# example 1
+# def net_price(list_price, discount=0, tax=0.05):
+#     return list_price * (1 - discount) * (1 + tax)
+
+# print(net_price(500))
+# print(net_price(500, 0.1, 0))
+
+# example 2
+import time
+
+
+def count(start, end):
+    for x in range(start, end+1):
+        print(x)
+        time.sleep(1)
+    print("DONE!")
+
+
+count(0, 10)
