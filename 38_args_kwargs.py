54c113587bbcdb3852494976d3ebf0600d94e9fe
18Prashanth <18gowda2002@gmail.com>
2025-01-25 18:56:55 +0000

    args_kwargs

--- 38_args_kwargs.py
+++ 38_args_kwargs.py
@@ -4,11 +4,19 @@
 # 1.positional 2.default 3.keyboard 4.ARBITRARY
 
 
-def add(*args):
-    total = 0
+# def add(*nums):
+#     total = 0
+#     for num in nums:
+#         total += num
+#     return total
+
+
+# print(add(1, 2, 3))
+
+# example 2
+def display_name(*args):
     for arg in args:
-        total += arg
-    return total
+        print(arg, end=" ")
 
 
-print(add(1))
+display_name("Mr", "Prashanth", "gowda")
