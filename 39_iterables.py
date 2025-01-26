9da221ce6b20def00e033567d0260ec46a35499f
18Prashanth <18gowda2002@gmail.com>
2025-01-26 12:14:56 +0000

    iterable

--- 39_iterables.py
+++ 39_iterables.py
@@ -1,7 +1,27 @@
 # Iterables = An object/collection thar can return its elements one at a time,
 # allowing it to be iterated over in a loop
 
-numbers = [1, 2, 3, 4, 5]
+# numbers = [1, 2, 3, 4, 5]
 
-for number in reversed(numbers):
-    print(number)
+# for number in reversed(numbers):
+#     print(number, end="-")
+
+
+# example - 2 --sets cannot be reverseble
+# fruits = {"apple", "orange", "banana", "coconut"}
+
+# for fruit in fruits:
+#     print(fruit)
+
+
+# example 3
+# name = "Prasi gowda"
+
+# for character in name:
+#     print(character, end="")
+
+# example 4
+my_dictionary = {"A": 1, "B": 2, "C": 3}
+
+for key, value in my_dictionary.items():
+    print(f"{key}: {value}")
