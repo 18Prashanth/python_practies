b93c1b253ea0c849ac08df0fe49c8572977fc77e
18Prashanth <18gowda2002@gmail.com>
2025-01-26 17:23:45 +0000

    Module

--- 43_Module.py
+++ 43_Module.py
@@ -5,4 +5,11 @@
 
 # import math
 # import math as m
-from math import pi
+# from math import pi
+from math import e
+
+a, b, c, d = 1, 2, 3, 4
+print(e ** a)
+print(e ** b)
+print(e ** c)
+print(e ** d)
