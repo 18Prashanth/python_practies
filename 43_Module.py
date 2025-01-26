4b74c1756f47567e2cfef54a14cb42fee8dd2766
18Prashanth <18gowda2002@gmail.com>
2025-01-26 17:10:26 +0000

    Module

---
+++ 43_Module.py
@@ -0,0 +1,8 @@
+# module = a file containing code you want to include in your program
+#          use 'import' to include a module (built-in or your own)
+#          useful to break up alarge program reusable seperate files
+
+
+# import math
+# import math as m
+from math import pi
