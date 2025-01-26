771c9ed9048bb47c68a82bf5f9abfa7de9d11ce4
18Prashanth <18gowda2002@gmail.com>
2025-01-26 18:30:38 +0000

    Module

--- 43_Module.py
+++ 43_Module.py
@@ -6,10 +6,20 @@
 # import math
 # import math as m
 # from math import pi
-from math import e
+# from math import e
 
-a, b, c, d = 1, 2, 3, 4
-print(e ** a)
-print(e ** b)
-print(e ** c)
-print(e ** d)
+# a, b, c, d = 1, 2, 3, 4
+# print(e ** a)
+# print(e ** b)
+# print(e ** c)
+# print(e ** d)
+
+# ---------------------------------\The below module import from the file 44_example43.py--
+import example
+
+# result = example43.pi()
+result = example.square(3)
+result = example.cube(3)
+result = example.circumference(3)
+result = example.area(3)
+print(result)
