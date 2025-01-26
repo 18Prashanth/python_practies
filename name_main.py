00ab03da8f163f8719c5e5fc1231e24d369788c7
18Prashanth <18gowda2002@gmail.com>
2025-01-26 19:29:25 +0000

    main

--- name_main.py
+++ name_main.py
@@ -1,3 +1,17 @@
-# if __name__==__main__: (this script can be impoerted OR run standalone)
+# if __name__=='__main__': (this script can be impoerted OR run standalone)
 #                         function and classes in this module can be reused
 #                         without the main block of code executing
+
+
+def favorite_food(food):
+    print(f"Your favorite food is {food}")
+
+
+def main():
+    print("This is script1")
+    favorite_food("pizza")
+    print("Goodbye!")
+
+
+if __name__ == '__main__':
+    main()
---
+++ script2.py
@@ -0,0 +1,16 @@
+from name_main import *
+
+
+def favorite_drink(drink):
+    print(f"Your favorite drink is {drink}")
+
+
+def main():
+    print("This is script2")
+    favorite_food("sushi")
+    favorite_drink("coffee")
+    print('Good bye!')
+
+
+if __name__ == '__main__':
+    main()
