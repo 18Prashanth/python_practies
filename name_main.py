10549a639da3b3bec5a1f592ac4c2ef1d0dd18a2
18Prashanth <18gowda2002@gmail.com>
2025-01-26 19:21:06 +0000

    main

---
+++ name_main.py
@@ -0,0 +1,3 @@
+# if __name__==__main__: (this script can be impoerted OR run standalone)
+#                         function and classes in this module can be reused
+#                         without the main block of code executing
