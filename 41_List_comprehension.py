d8962ca6ac1fb847a4a00f1267f139515f1f2f58
18Prashanth <18gowda2002@gmail.com>
2025-01-26 13:55:15 +0000

    List comprehension

--- 41_List_comprehension.py
+++ 41_List_comprehension.py
@@ -10,5 +10,22 @@
 
 
 # example 2
-doubles = [x * 2 for x in range(1, 11)]
-print(doubles)
+# doubles = [x * 2 for x in range(1, 11)]
+# triples = [y * 3 for y in range(1, 11)]
+# squares = [z * 3 for z in range(1, 11)]
+
+# print(squares)
+
+# fruits = ["apple", "orange", "banana"]
+# fruit_chars = [fruit[0] for fruit in fruits]
+
+# print(fruit_chars)
+
+
+# example ---
+
+numbers = [1, -2, -3, -4, -5, -6]
+positive_nums = [num for num in numbers if num >= 0]
+negative_nums = [num for num in numbers if num < 0]
+even_nums = [num for num in numbers if num % 2 == 0]
+print(even_nums)
