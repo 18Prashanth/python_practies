5bc01eda89a999d75e5ab290365cedb9b307a98f
18Prashanth <18gowda2002@gmail.com>
2025-01-28 13:47:54 +0000

    Banking

--- 45_Banking-program.py
+++ 45_Banking-program.py
@@ -1,11 +1,15 @@
 # Python Banking Program
 
-def show_balance():
+def show_balance(balance):
+    print("*******************")
     print(f"Your balance is ${balance:.2f}")
+    print("*******************")
 
 
 def deposit():
+    print("*******************")
     amount = float(input("Enter an amount to be deposited: "))
+    print("*******************")
 
     if amount < 0:
         print("Enter a valid amount")
@@ -15,42 +19,57 @@ def deposit():
         return amount
 
 
-def withdraw():
+def withdraw(balance):
+    print("*******************")
     amount = int(input("Enter amount to be withdraw: "))
+    print("*******************")
 
     if amount > balance:
+        print("*******************")
         print("Insufficient fund")
+        print("*******************")
         return 0
     elif amount < 0:
+        print("*******************")
         print("Amount must be greater than 0")
+        print("*******************")
         return 0
     else:
         return amount
 
 
-balance = 0
-is_running = True
+def main():
+    balance = 0
+    is_running = True
+
+    while is_running:
+        print("*******************")
+        print("Banking program")
+        print("*******************")
+        print("1. Show Balance")
+        print("2. Deposit")
+        print("3. Withdraw")
+        print("4. Exit")
+        print("*******************")
+        choice = input("Enter your choice (1-4): ")
+
+        if choice == '1':
+            show_balance(balance)
+        elif choice == '2':
+            balance += deposit()
+        elif choice == '3':
+            balance -= withdraw(balance)
+        elif choice == '4':
+            is_running = False
+        else:
+            print("*******************")
+            print("That is not a valid test")
+            print("*******************")
+
+    print("*******************")
+    print("Thank you have a nice day!")
+    print("*******************")
 
 
-while is_running:
-    print("Banking program")
-    print("1. Show Balance")
-    print("2. Deposit")
-    print("3. Withdraw")
-    print("4. Exit")
-
-    choice = input("Enter your choice (1-4): ")
-
-    if choice == '1':
-        show_balance()
-    elif choice == '2':
-        balance += deposit()
-    elif choice == '3':
-        balance -= withdraw()
-    elif choice == '4':
-        is_running = False
-    else:
-        print("That is not a valid test")
-
-
-print("Thank you have a nice day!")
+if __name__ == "__main__":
+    main()
