fe1dc895c8d34583fb8041b747f5f6fc85b5977e
18Prashanth <18gowda2002@gmail.com>
2025-01-29 14:09:17 +0000

    obeject-oriented-programming

--- 49_oops.py
+++ 49_oops.py
@@ -4,25 +4,21 @@
 
 # class = (blueprint) used to design the structure and layout of an object
 
-class car:
-    def __init__(self, model, year, color, for_sale):
-        self.model = model
-        self.year = year
-        self.color = color
-        self.for_sale = for_sale
-
+from car import car
 
 car1 = car("Mustang", 2024, "red", False)
 car2 = car("Corvvette", 2025, "blue", True)
 
-print(car1.model)
-print(car1.year)
-print(car1.color)
-print(car1.for_sale)
+# print(car1.model)
+# print(car1.year)
+# print(car1.color)
+# print(car1.for_sale)
 
-print()
+# print()
 
-print(car1.model)
-print(car1.year)
-print(car1.color)
-print(car1.for_sale)
+# print(car1.model)
+# print(car1.year)
+# print(car1.color)
+# print(car1.for_sale)
+
+car1.stop()
