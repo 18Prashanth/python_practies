85c579d2cf9ab9e39411e2e947c32148955caab9
18Prashanth <18gowda2002@gmail.com>
2025-01-31 20:32:38 +0000

    mutilple and multi level inheritance

---
+++ 52_multiple_inheritance.py
@@ -0,0 +1,24 @@
+# Multiple inheritance = inherit from more than one parent class
+#                       C(A,B)
+
+# Multi level inheritance = inherit from a parent which inherits from another parent()
+#                            C(B) <- B(A) <- A
+
+class prey:
+    pass
+
+
+class Predator:
+    pass
+
+
+class Rabbit:
+    pass
+
+
+class Hawk:
+    pass
+
+
+class Fish:
+    pass
