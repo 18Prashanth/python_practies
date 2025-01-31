e1322599ba67577f0c6018a0ebf28020c2712f58
18Prashanth <18gowda2002@gmail.com>
2025-01-31 20:03:18 +0000

    class variables

--- 50_class_variables.py
+++ 50_class_variables.py
@@ -2,14 +2,25 @@
 # defined outside the constructor
 # Allow you to share data among all objects created from that class
 
-class student:
+class Student:
+
+    class_year = 2025
+    num_students = 0
+
     def __init__(self, name, age):
         self.name = name
         self.age = age
+        Student.num_students += 1
 
 
-student1 = student("Prashanth", 30)
-student2 = student("Patrick", 35)
+student1 = Student("Prashanth", 30)
+student2 = Student("Patrick", 35)
+student3 = Student("Shivakumar", 55)
+student4 = Student("Sandy", 27)
 
+print(f"My graduating class of {Student.class_year} has {
+      Student.num_students} students")
 print(student1.name)
-print(student2.age)
+print(student2.name)
+print(student3.name)
+print(student4.name)
