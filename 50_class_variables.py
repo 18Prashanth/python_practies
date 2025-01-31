27f67f19d080755b6bf0930f84668732fe169684
18Prashanth <18gowda2002@gmail.com>
2025-01-31 19:54:38 +0000

    class variables

--- 50_class_variables.py
+++ 50_class_variables.py
@@ -1,3 +1,15 @@
 # class variables = shared among all instances of a class
 # defined outside the constructor
 # Allow you to share data among all objects created from that class
+
+class student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+student1 = student("Prashanth", 30)
+student2 = student("Patrick", 35)
+
+print(student1.name)
+print(student2.age)
