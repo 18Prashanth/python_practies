e66257753180d9f11c61cf4190a566575240d55c
18Prashanth <18gowda2002@gmail.com>
2025-02-02 17:48:41 +0000

    Static method

---
+++ 56_static_method.py
@@ -0,0 +1,4 @@
+# static methods = A method that belong to a class rather than any object from that class (instances)
+# usually used for general utility function
+# Instance methods = Best for operation on instances of the class (objects)
+# Static methods = Best for utility functions that do not need access to class data
