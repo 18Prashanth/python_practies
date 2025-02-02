624a0eaf7100eae1ca8784607d6db68ed71a0569
18Prashanth <18gowda2002@gmail.com>
2025-02-02 13:57:38 +0000

    duck typing

---
+++ 55_duck_typing.py
@@ -0,0 +1,3 @@
+# "Duck typing" = Another way to achive polymorphism besides Inheritance
+# object mustr have the minimum necessary attributes/methods
+# "If it looks like a duck and quacks like a duck, it must be a duck"
