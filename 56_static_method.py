df9c6eec1f20012fb5e57a37642e92095cc0a52f
18Prashanth <18gowda2002@gmail.com>
2025-02-02 17:55:41 +0000

    Static method

--- 56_static_method.py
+++ 56_static_method.py
@@ -2,3 +2,20 @@
 # usually used for general utility function
 # Instance methods = Best for operation on instances of the class (objects)
 # Static methods = Best for utility functions that do not need access to class data
+
+class Employee:
+
+    def __init__(self, name, position):
+        self.name = name
+        self.position = position
+
+    def get_info(self):
+        return f"{self.name} = {self.position}"
+
+    @staticmethod
+    def is_valid_position(position):
+        valid_positions = ["manager", "cashier", "cook", "Janitor"]
+        return position in valid_positions
+
+
+print(Employee.is_valid_position("cook"))
