03fe3bdfd932f7944fa168328a3ec66222f4db34
18Prashanth <18gowda2002@gmail.com>
2025-02-03 20:02:58 +0000

    Magic method

--- 58_Magic_method.py
+++ 58_Magic_method.py
@@ -9,7 +9,38 @@ class Book:
         self.author = author
         self.num_pages = num_pages
 
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+    def __eq__(self, other):
+        return self.title == other.title and self.author == other.author
+
+    def __lt__(self, other):
+        return self.num_pages < other.num_pages
+
+    def __gt__(self, other):
+        return self.num_pages > other.num_pages
+
+    def __add__(self, other):
+        return f"{self.num_pages + other.num_pages} pages"
+
+    def __contains__(self, keyword):
+        return keyword in self.title or keyword in self.author
+
+    def __getline__(self, key):
+        if key == "title":
+            return self.title
+        elif key == "author":
+            return self.author
+        elif key == "num_pages":
+            return self.num_pages
+        else:
+            return f"key {key} was not found"
+
 
 book1 = Book("The Hobbit", "J.R.R. Tolkien", 310)
-book2 = Book("Harry Potter and The Philosopher's Stone", "J.K. Rowling", 223)
+book2 = Book("The Hobbit", "J.R.R. Tolkien", 310)
 book3 = Book("The Lion, the Witch and the Wardrobe", "C.S. Lewis", 172)
+
+
+print(book1['Lion'])
