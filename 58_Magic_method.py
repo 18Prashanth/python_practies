32c1a7b4fb434d3c7035117d83c39ce5790c0e59
18Prashanth <18gowda2002@gmail.com>
2025-02-03 13:22:48 +0000

    Magic method

---
+++ 58_Magic_method.py
@@ -0,0 +1,15 @@
+# Magic methods = Dunder methods(double underscore) __init__, __str__, __eq__
+# They are automatically called by many of python's biult-in operations.
+# They allow developers to define or customize the behavior of objects
+
+class Book:
+
+    def __init__(self, title, author, num_pages):
+        self.title = title
+        self.author = author
+        self.num_pages = num_pages
+
+
+book1 = Book("The Hobbit", "J.R.R. Tolkien", 310)
+book2 = Book("Harry Potter and The Philosopher's Stone", "J.K. Rowling", 223)
+book3 = Book("The Lion, the Witch and the Wardrobe", "C.S. Lewis", 172)
