b46fa8234241ba04c41f123d8afe6acb1b194fde
18Prashanth <18gowda2002@gmail.com>
2025-02-04 16:20:04 +0000

    exception

--- 61_Exception_handling.py
+++ 61_Exception_handling.py
@@ -1,3 +1,13 @@
 # Exception = An event that interrupts the flow of a program
-# (ZeroDivisionError, TypeError, ValueError)
-# 1.try, 2.except, 3.finally
+#             (ZeroDivisionError, TypeError, ValueError)
+#              1.try, 2.except, 3.finally
+
+try:
+    number = int(input("Enter a number: "))
+    print(1 / number)
+except ZeroDivisionError:
+    print("You can't divide by zero")
+except ValueError:
+    print("Enter only numbers please")
+except Exception:
+    print("Somethimg went wrong")
