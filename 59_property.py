5293fda9474796e0fb29a9bbef1619f83b8abf48
18Prashanth <18gowda2002@gmail.com>
2025-02-04 11:35:38 +0000

    property

---
+++ 59_property.py
@@ -0,0 +1,3 @@
+# @property = Decorator used to defin a method as a property (it can be accessed like an attribute)
+# Benifit: Add adfditional logic when read, write, or delete attribute
+# Gives you getter, setter, and deleter method
