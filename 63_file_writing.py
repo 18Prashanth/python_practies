a906fec4feba8a68d0d4b9db6abd951bf802319f
18Prashanth <18gowda2002@gmail.com>
2025-02-05 13:25:34 +0000

    File writing

--- 63_file_writing.py
+++ 63_file_writing.py
@@ -1,12 +1,29 @@
 # Python writting files(.txt, .json, .csv)
 
-txt_data = "I Like pizza"
+# txt_data = "I Like pizza!"
+import json
+import csv
+# employee = {
+#     "name": "Spongebob",
+#     "age": 30,
+#     "job": "cook"
+# }
+employee = [["Name", "Age", "Job"],
+            ["Prashanth", 23, "SDE"],
+            ["Shivakumar", 50, "NGO"],
+            ["Joo", 22, "Web developer"]]
 
-file_path = "C:/Users/Admin/OneDrive/Desktop/test/test.txt"
+employees = ["Eugene", "Squidward", "Spongebob", "Patrick"]
+
+file_path = "C:/Users/Admin/OneDrive/Desktop/test/test3.csv"
 
 try:
-    with open(file_path, "x") as file:  # if we open a file 'with' its atuomatically close the file
-        file.write(txt_data)
-        print(f"txt file '{file_path}' was created")
+    # if we open a file 'with' its atuomatically close the file
+    with open(file_path, "w", newline="") as file:
+        # json.dump(employee, file, indent=4)
+        writer = csv.writer(file)
+        for row in employee:
+            writer.writerow(row)
+        print(f"csv file '{file_path}' was created")
 except FileExistsError:
-    print("File already exist")
+    print("That file already exist")
