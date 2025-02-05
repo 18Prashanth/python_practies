c0d5c08651ca4156c7859f7905969368e4f5692f
18Prashanth <18gowda2002@gmail.com>
2025-02-05 21:33:13 +0000

    Multi-threading

--- 67_multiple_threading.py
+++ 67_multiple_threading.py
@@ -6,9 +6,9 @@ import threading
 import time
 
 
-def walk_dog():
+def walk_dog(first, last):
     time.sleep(8)
-    print("You finish walking the dog")
+    print(f"You finish walking {first} {last}")
 
 
 def take_out_trash():
@@ -21,6 +21,21 @@ def get_mail():
     print("You get the mail")
 
 
-walk_dog()
-take_out_trash()
-get_mail()
+# walk_dog()
+# take_out_trash()
+# get_mail()
+
+chore1 = threading.Thread(target=walk_dog, args=("Scoby", "Doo"))
+chore1.start()
+
+chore2 = threading.Thread(target=take_out_trash)
+chore2.start()
+
+chore3 = threading.Thread(target=get_mail)
+chore3.start()
+
+chore1.join()
+chore2.join()
+chore3.join()
+
+print("All chors are complete!")
