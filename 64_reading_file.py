04d7ae60e33106cf489c7a843c3843a8fb56818d
18Prashanth <18gowda2002@gmail.com>
2025-02-05 16:00:12 +0000

    reading file

--- 64_reading_file.py
+++ 64_reading_file.py
@@ -1,14 +1,17 @@
 # python reading files (.txt, .json, .csv)
 
 import json
+import csv
 
-file_path = "C:/Users/Admin/OneDrive/Desktop/test/test2.json"
+file_path = "C:/Users/Admin/OneDrive/Desktop/test/test3.csv"
 
 try:
     with open(file_path, "r") as file:
         # content = file.read()
-        content = json.load(file)
-        print(content)
+        # content = json.load(file)
+        content = csv.reader(file)
+        for line in content:
+            print(line)
 except FileNotFoundError:
     print("That file was not found")
 except PermissionError:
