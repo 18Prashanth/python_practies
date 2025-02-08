c5a7d8505083710bc74afe08ecead7f69de6f10e
18Prashanth <18gowda2002@gmail.com>
2025-02-08 11:48:01 +0000

    Radio button

--- 74_radio_buttons.py
+++ 74_radio_buttons.py
@@ -19,12 +19,19 @@ class MainWindow(QMainWindow):
         self.radio2.setGeometry(0, 50, 300, 50)
         self.radio3.setGeometry(0, 100, 300, 50)
         self.radio4.setGeometry(0, 150, 300, 50)
+        self.button_group1 = QButtonGroup(self)
+        self.button_group2 = QButtonGroup(self)
 
         self.setStyleSheet("QRadioButton{"
                            "font-size: 40px;"
                            "font-family: Arial;"
                            "padding: 10px;"
                            "}")
+
+        self.button_group1.addButton(self.radio1)
+        self.button_group1.addButton(self.radio2)
+        self.button_group2.addButton(self.radio3)
+        self.button_group2.addButton(self.radio4)
 
 
 if __name__ == "__main__":
