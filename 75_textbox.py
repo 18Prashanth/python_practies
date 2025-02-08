f9a6b8acc3840233a7e9c545690c6bb2c35f4a42
18Prashanth <18gowda2002@gmail.com>
2025-02-08 12:07:01 +0000

    textbox

--- 75_textbox.py
+++ 75_textbox.py
@@ -1,15 +1,30 @@
+# line edit
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit, QPushButton
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setGeometry(700, 300, 500, 500)
+        self.line_edit = QLineEdit(self)
+        self.button = QPushButton("Submit", self)
         self.initUI()
 
     def initUI(self):
-        pass
+        self.line_edit.setGeometry(10, 10, 200, 40)
+        self.button.setGeometry(210, 10, 100, 40)
+        self.line_edit.setStyleSheet("font-size:25px;"
+                                     "font-family: Arial;")
+
+        self.button.setStyleSheet("font-size:20px;"
+                                  "font-family: Arial;")
+        self.line_edit.setPlaceholderText("Enter yoyr name")
+        self.button.clicked.connect(self.submit)
+
+    def submit(self):
+        text = self.line_edit.text()
+        print(f"Hello {text}")
 
 
 if __name__ == '__main__':
