aeae6a655d57f13ae2a26a0dd78a0372af6e0fe8
18Prashanth <18gowda2002@gmail.com>
2025-02-15 14:13:23 +0000

    updated

--- pandas/1_intro.py
+++ pandas/1_intro.py
@@ -1,3 +1,11 @@
 # introduction to pandas
 
 import pandas as pd
+# a = [1, 7, 2]
+
+# myvar = pd.Series(a, index=["x", "y", "z"])
+
+fruites = {"apple": 20, "orange": 30, "banana": 20}
+
+fruit_tabel = pd.Series(fruites, index=["apple", "orange"])
+print(fruit_tabel)
